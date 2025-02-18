35f06f961628fe391a8b9805e6209dffbbdce73b
Edward Koikai <edwardkoikai@gmail.com>
2025-02-18 09:50:36 +0300

    UPDATE student_management: refine module manifest with updated metadata and structure

--- custom_addons/student_management/__manifest__.py
+++ custom_addons/student_management/__manifest__.py
@@ -1,21 +1,19 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "student_management",
-
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'name': 'Student Management',
+    'summary': "A module for managing students and courses",
 
     'description': """
 Long description of module's purpose
     """,
 
-    'author': "My Company",
+    'author': "Edd Dev",
     'website': "https://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'Education',
+    'version': '1.0',
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
@@ -23,12 +21,14 @@ Long description of module's purpose
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'views/student_view.xml',
+        # 'views/templates.xml',
+        'security/ir.model.access.csv'
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    # 'demo': [
+    #     # 'demo/demo.xml',
+    # ],
+    'installable': True,
+    'application': True,
 }
-
